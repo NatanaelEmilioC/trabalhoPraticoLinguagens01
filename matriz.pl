42c78c4e9673277769647f578c5fdea6e65885de
natanaelemilio <natanaelemilio@gmail.com>
2019-06-01 18:42:27 -0300

    alterações implementadas

--- matriz.pl
+++ matriz.pl
@@ -1,107 +1,101 @@
-%Cria uma matriz MxN qualquer com um numero predefinido
-%Nr é o número de linhas da matriz
-%Nc é o número de colunas da matriz
-%N é o número com que a matriz é preenchida
-createMatrix(0,_,_,[]).
-createMatrix(Nr,Nc,N,[R|Rtail]) :- createRow(Nc,N,R), Nx is Nr-1, createMatrix(Nx,Nc,N,Rtail).
-createRow(0,_,[]).
-createRow(Nc,N,[N|Aux]) :- Ncaux is Nc-1, createRow(Ncaux,N,Aux).
-
 %Recupera um elemento da matriz
 %R é a linha do elemento requerido
 %C é a coluna do elemento requerido
 %E é o retorno(Elemento posicionado em RC)
-getElement(R,C,M,E) :- nth1(R,M,Row),nth1(C,Row,E).
+pegaElemento(R,C,Matriz,E) :- nth1(R,Matriz,Coluna),nth1(C,Coluna,E).
 
 %Verifica se a matriz ainda não alcançou o estado final
-%Pr é a linha em que o marcador se encontra
-%Pc é a coluna em que o marcador se encontra
-%M é a matriz que se deseja avaliar
-matrixVerify(Pr,Pc,M) :- matrixVerify(Pr,Pc,1,M).
-matrixVerify(Pr,Pc,Pr,[R|_]) :- rowVerify(Pc,R),!.
-matrixVerify(Pr,_,I,[R|_]) :- Pr =\= I, rowVerify(-1,R),!.
-matrixVerify(Pr,Pc,I,[_|Rs]) :- Ni is I+1, matrixVerify(Pr,Pc,Ni,Rs).
-rowVerify(Pc,R) :- rowVerify(Pc,1,R).
-rowVerify(Pc,I,[R|_]) :- Pc =\= I, R =\= -1,!.
-rowVerify(Pc,I,[_|Rs]):- Ni is I+1, rowVerify(Pc,Ni,Rs).
+%Linha é a linha em que o marcador se encontra
+%Coluna é a coluna em que o marcador se encontra
 
-%Insere elemento na matriz na posição Pr,Pc e retorna a nova matriz resultante
-%Parâmetros: (Elemento, Pr,Pc, Matriz, NovaMatriz)
-insert_matrix(_,_,_,[],[]).
-insert_matrix(Elem,1,1,[[_|Xs]|M1],[[Elem|Xs1]|M2]) :- !, insert_matrix(Elem,0,0,[Xs|M1],[Xs1|M2]).
-insert_matrix(Elem,0,0,[[X]|Xs],[[X]|Xs1]) :- insert_matrix(Elem,0,0,Xs,Xs1), !.
-insert_matrix(Elem,0,0,[[X|Xs]|M1],[[X|Xs1]|M2]) :- insert_matrix(Elem,0,0,[Xs|M1],[Xs1|M2]).
-insert_matrix(Elem,1,Pc,[[X|Xs]|M1],[[X|Xs1]|M2]) :- PcAux is Pc-1, insert_matrix(Elem,1,PcAux,[Xs|M1],[Xs1|M2]). 
-insert_matrix(Elem,Pr,Pc,[Xs|M1],[Xs|M2]) :- PrAux is Pr-1, insert_matrix(Elem,PrAux,Pc,M1,M2), !.
+verificaMatriz(Linha,Coluna,Matriz) :- verificaMatriz(Linha,Coluna,1,Matriz).
+verificaMatriz(Linha,Coluna,Linha,[R|_]) :- verificaColuna(Coluna,R),!.
+verificaMatriz(Linha,_,I,[R|_]) :- Linha =\= I, verificaColuna(-1,R),!.
+verificaMatriz(Linha,Coluna,I,[_|Rs]) :- Ni is I+1, verificaMatriz(Linha,Coluna,Ni,Rs).
+verificaColuna(Coluna,R) :- verificaColuna(Coluna,1,R).
+verificaColuna(Coluna,I,[R|_]) :- Coluna =\= I, R =\= -1,!.
+verificaColuna(Coluna,I,[_|Rs]):- Ni is I+1, verificaColuna(Coluna,Ni,Rs).
 
-%Confere se a posição(Pr,Pc) é uma posição válida para a matriz
-%Pr é a linha
-%Pc é a coluna
-%M é a matriz
-isValidPosition(Pr,Pc,M) :- length(M,PrMax),
-                            Pr =< PrMax,
-                            Pr > 0,
-                            nth1(1,M,FirstRow),
-                            length(FirstRow,PcMax),
-                            Pc =< PcMax,
-                            Pc > 0,
-                            getElement(Pr,Pc,M,Elem),
-                            Elem >= 0.
+%Insere elemento na matriz na posição Linha,Coluna e retorna a nova matriz resultante
+%Parâmetros: (Elemento, Linha,Coluna, Matriz, NovaMatriz)
+inserirMatriz(_,_,_,[],[]).
+inserirMatriz(Elemento,1,1,[[_|Xs]|M1],[[Elemento|Xs1]|M2]) :- !, inserirMatriz(Elemento,0,0,[Xs|M1],[Xs1|M2]).
+inserirMatriz(Elemento,0,0,[[X]|Xs],[[X]|Xs1]) :- inserirMatriz(Elemento,0,0,Xs,Xs1), !.
+inserirMatriz(Elemento,0,0,[[X|Xs]|M1],[[X|Xs1]|M2]) :- inserirMatriz(Elemento,0,0,[Xs|M1],[Xs1|M2]).
+inserirMatriz(Elemento,1,Coluna,[[X|Xs]|M1],[[X|Xs1]|M2]) :- PcAux is Coluna-1, inserirMatriz(Elemento,1,PcAux,[Xs|M1],[Xs1|M2]). 
+inserirMatriz(Elemento,Linha,Coluna,[Xs|M1],[Xs|M2]) :- PrAux is Linha-1, inserirMatriz(Elemento,PrAux,Coluna,M1,M2), !.
 
-%Move para baixo
-moveDown(Pr,Pc,M,Cs) :- PrNext is Pr+1,
-                        isValidPosition(PrNext,Pc,M),
-                        getElement(Pr,Pc,M,Elem),  
-                        Eaux is Elem - 1,    
-                        insert_matrix(Eaux,Pr,Pc,M,Mod),!,
-                        move(PrNext,Pc,Mod,Cs).
-%Move para direita  
-moveRight(Pr,Pc,M,Cs) :- PcNext is Pc+1,
-                         isValidPosition(Pr,PcNext,M),
-                         getElement(Pr,Pc,M,Elem),
-                         Eaux is Elem - 1,    
-                         insert_matrix(Eaux,Pr,Pc,M,Mod),!,
-                         move(Pr,PcNext,Mod,Cs).
-%Move para cima
-moveUp(Pr,Pc,M,Cs) :- PrNext is Pr-1,
-                      isValidPosition(PrNext,Pc,M),    
-                      getElement(Pr,Pc,M,Elem),
-                      Eaux is Elem - 1,    
-                      insert_matrix(Eaux,Pr,Pc,M,Mod),!,
-                      move(PrNext,Pc,Mod,Cs).
-%Move para esquerda
-moveLeft(Pr,Pc,M,Cs) :- PcNext is Pc-1,
-                        isValidPosition(Pr,PcNext,M),
-                        getElement(Pr,Pc,M,Elem),
-                        Eaux is Elem - 1,    
-                        insert_matrix(Eaux,Pr,Pc,M,Mod),!,                                 
-                        move(Pr,PcNext,Mod,Cs).
+%Confere se a posição(Linha,Coluna) é uma posição válida para a matriz
+posicaoValida(Linha,Coluna,Matriz) :- length(Matriz,PrMax),
+                            Linha =< PrMax,
+                            Linha > 0,
+                            nth1(1,Matriz,FirstColuna),
+                            length(FirstColuna,PcMax),
+                            Coluna =< PcMax,
+                            Coluna > 0,
+                            pegaElemento(Linha,Coluna,Matriz,Elemento),
+                            Elemento >= 0.
 
-move(Pr,Pc,M,["End"]) :- \+matrixVerify(Pr,Pc,M).
-move(Pr,Pc,M,["Down"|C]) :- moveDown(Pr,Pc,M,C).
-move(Pr,Pc,M,["Right"|C]) :- moveRight(Pr,Pc,M,C).
-move(Pr,Pc,M,["Up"|C]) :- moveUp(Pr,Pc,M,C).
-move(Pr,Pc,M,["Left"|C]) :- moveLeft(Pr,Pc,M,C).
+%mover para baixo
+moverBaixo(Linha,Coluna,Matriz,Cs) :- PrNext is Linha+1,
+                        posicaoValida(PrNext,Coluna,Matriz),
+                        pegaElemento(Linha,Coluna,Matriz,Elemento),  
+                        Eaux is Elemento - 1,    
+                        inserirMatriz(Eaux,Linha,Coluna,Matriz,Mod),!,
+                        mover(PrNext,Coluna,Mod,Cs).
+%mover para direita  
+moverDireita(Linha,Coluna,Matriz,Cs) :- PcNext is Coluna+1,
+                         posicaoValida(Linha,PcNext,Matriz),
+                         pegaElemento(Linha,Coluna,Matriz,Elemento),
+                         Eaux is Elemento - 1,    
+                         inserirMatriz(Eaux,Linha,Coluna,Matriz,Mod),!,
+                         mover(Linha,PcNext,Mod,Cs).
+%mover para cima
+moverCima(Linha,Coluna,Matriz,Cs) :- PrNext is Linha-1,
+                      posicaoValida(PrNext,Coluna,Matriz),    
+                      pegaElemento(Linha,Coluna,Matriz,Elemento),
+                      Eaux is Elemento - 1,    
+                      inserirMatriz(Eaux,Linha,Coluna,Matriz,Mod),!,
+                      mover(PrNext,Coluna,Mod,Cs).
+%mover para esquerda
+moverEsquerda(Linha,Coluna,Matriz,Cs) :- PcNext is Coluna-1,
+                        posicaoValida(Linha,PcNext,Matriz),
+                        pegaElemento(Linha,Coluna,Matriz,Elemento),
+                        Eaux is Elemento - 1,    
+                        inserirMatriz(Eaux,Linha,Coluna,Matriz,Mod),!,                                 
+                        mover(Linha,PcNext,Mod,Cs).
 
-%Predicado para iniciar o jogo
-%R é o número de linhas da matriz
-%C é o número de colunas da matriz
-%N é o número com que a matriz inicial será preenchida
-%Ri é a linha da posição inicial do jogador
-%Ci é a coluna da posição inicial do jogador
-%Ways são os caminhos encontrados
+mover(Linha,Coluna,Matriz,[".."]) :- \+verificaMatriz(Linha,Coluna,Matriz).
+mover(Linha,Coluna,Matriz,["Embaixo"|C]) :- moverBaixo(Linha,Coluna,Matriz,C).
+mover(Linha,Coluna,Matriz,["Direita"|C]) :- moverDireita(Linha,Coluna,Matriz,C).
+mover(Linha,Coluna,Matriz,["Cima"|C]) :- moverCima(Linha,Coluna,Matriz,C).
+mover(Linha,Coluna,Matriz,["Esquerda"|C]) :- moverEsquerda(Linha,Coluna,Matriz,C). 
 
-jogar(R,C,N,Ri,Ci,Ways) :- createMatrix(R,C,N,M),!,move(Ri,Ci,M,Ways).           
+executar(Matriz,LinhaI,ColunaI,Caminhos) :-
+    soGravaEmArquivo((findall(Caminhos,mover(LinhaI,ColunaI,Matriz,Caminhos),C),
+    !,
+    length(C, T), 
+    write(C), 
+    write(T)),
+    'novaMatriz.txt').
 
-readFileSee(InputFile, OutputFile) :-
-    seeing(OldStream),
-    see(InputFile),
-    tell(OutputFile),
+soGravaEmArquivo(Procedimento,'novaMatriz.txt') :-
+    seeing(Fluxo),
+    tell('novaMatriz.txt'),
+    Procedimento,
+    told,
+    see(Fluxo).
+
+/*
+executar :- leArquivos('matriz.txt', 'novaMatriz.txt').
+
+leArquivos(Entrada, Saida) :-
+    seeing(Fluxo),
+    see(Entrada),
+    tell(Saida),
     repeat,
     read(Term),
     ( Term == end_of_file -> true ; Term, fail),
     seen,
     told,
-    see(OldStream).
-
-executar :- readFileSee('matriz.txt', 'novaMatriz.txt').
+    see(Fluxo).*/
